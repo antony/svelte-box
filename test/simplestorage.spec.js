384a0cca6cd1a6987f8ec1866b7dd216b14eaa01
Antony Jones <aj@desirableobjects.co.uk>
2017-08-28 23:12:03 +0100

    start working on test support

---
+++ test/simplestorage.spec.js
@@ -0,0 +1,14 @@
+const SimpleStorage = artifacts.require("./SimpleStorage.sol")
+
+contract('SimpleStorage', function(accounts) {
+  it('...should store the value 89.', () => {
+    return SimpleStorage.deployed().then(function(instance) {
+      simpleStorageInstance = instance;
+      return simpleStorageInstance.set(89, {from: accounts[0]})
+    }).then(function() {
+      return simpleStorageInstance.get.call()
+    }).then(function(storedData) {
+      assert.equal(storedData, 89, "The value 89 was not stored.")
+    })
+  })
+})
